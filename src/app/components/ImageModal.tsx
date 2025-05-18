2ef300ab6f63316d85d0717f0bc0ab524e5008fb
Kenneth Doerflein <ktd500@verizon.net>
2025-05-18 15:50:46 -0400

    switch to Image from "next/image"

--- src/app/components/ImageModal.tsx
+++ src/app/components/ImageModal.tsx
@@ -1,12 +1,31 @@
-import { Modal, Image } from "react-bootstrap";
+import { Modal, Spinner } from "react-bootstrap";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
-const ImageModal = ({ show, onHide, src }: { show: boolean; onHide: () => void; src: string }) => (
-  <Modal show={show} onHide={onHide} fullscreen>
-    <Modal.Header closeButton />
-    <Modal.Body className="d-flex justify-content-center align-items-center bg-dark">
-      <Image src={src} fluid alt="Expanded" />
-    </Modal.Body>
-  </Modal>
-);
+const ImageModal = ({ show, onHide, src }: { show: boolean; onHide: () => void; src: string }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (show) setLoading(true);
+  }, [show, src]);
+
+  return (
+    <Modal show={show} onHide={onHide} fullscreen>
+      <Modal.Header closeButton />
+      <Modal.Body className="d-flex justify-content-center align-items-center bg-dark position-relative">
+        {loading && <Spinner animation="border" variant="light" />}
+        <div style={{ position: "relative", width: "100%", height: "80vh" }}>
+          <Image
+            src={src}
+            alt="Expanded"
+            fill
+            style={{ objectFit: "contain", display: loading ? "none" : "block" }}
+            onLoadingComplete={() => setLoading(false)}
+          />
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
 
 export default ImageModal;
