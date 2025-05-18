e07bafa0d75c1d06ffd6e424a7bd635c0f3c14da
Kenneth Doerflein <ktd500@verizon.net>
2025-05-18 15:51:13 -0400

    rename scroll to top button

--- src/app/components/ScrollToTopButton.tsx
+++ src/app/components/ScrollToTopButton.tsx
@@ -1,11 +1,11 @@
 import { Button } from "react-bootstrap";
 
 const ScrollToTopButton = () => (
-  <div className="text-center mt-4 scrollToTop">
+  <div className="text-center mt-4 goToBtnWrapper">
     <Button
       variant="info"
       className="rounded-circle"
-      id="scrollToTop"
+      id="goToBtn"
       title="Back to Top"
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
       <i className="bi bi-arrow-up"></i>
