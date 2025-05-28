efa95c3cd92170bd0dee51fdc9102fe4d0af2fd2
Kenneth Doerflein <ktd500@verizon.net>
2025-05-28 03:01:43 -0400

    Switch scroll to top button to use bootstrap tool tip

--- src/app/components/ScrollToTopButton.tsx
+++ src/app/components/ScrollToTopButton.tsx
@@ -1,16 +1,18 @@
-import { Button } from "react-bootstrap";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const ScrollToTopButton = () => (
   <div className="text-center mt-4 goToBtnWrapper">
-    <Button
-      variant="info"
-      className="rounded-circle"
-      id="goToBtn"
-      title="Back to Top"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
-      <i className="bi bi-arrow-up"></i>
-    </Button>
+    <OverlayTrigger
+      placement="left"
+      overlay={<Tooltip id="scroll-to-top-tooltip">Back to Top</Tooltip>}>
+      <Button
+        variant="info"
+        className="rounded-circle"
+        id="goToBtn"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+        <i className="bi bi-arrow-up"></i>
+      </Button>
+    </OverlayTrigger>
   </div>
 );
-
 export default ScrollToTopButton;
