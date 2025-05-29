abc947d8f2e9d10f78bc1c3f1360140f6825ccaf
Kenneth Doerflein <ktd500@verizon.net>
2025-05-29 16:13:58 -0400

    Add printables logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -17,6 +17,43 @@ export default function Footer({ appInfo }) {
         <i className="bi bi-github"></i>
       </Button>
 
+      <Button
+        variant="dark"
+        size="lg"
+        href="https://www.printables.com/@ken_590448"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-10 10 218.364 297.474"
+          width="24"
+          height="24"
+          className="block">
+          <g>
+            <path
+              fill="#fa6934"
+              d="M 100 0 C 92.596 -0.274 93.107 114.745 100 115 L 200 57 L 100 0 Z"
+            />
+            <path fill="#fa6934" d="M 0 172.474 L 0 287.474 L 100 229.474 L 0 172.474 Z" />
+            <path
+              fill="#fa6934"
+              d="M 0 0 C -6.355 -0.771 -7.465 113.966 0 115 L 100 57 L 0 0 Z"
+              transform="matrix(-1, 0, 0, -1, 100, 115)"
+            />
+            <path
+              fill="#fa6934"
+              d="M 100 57 L 100 172 C 104.237 182.064 204.237 124.064 200 114 C 200.363 95.923 106.16 50.129 100 57 Z"
+              transform="matrix(-1, 0, 0, -1, 300, 229)"
+            />
+            <path
+              fill="#fa6934"
+              d="M 100 115 L 100 230 L 200 172 C 203.364 169.57 106.006 110.662 100 115 Z"
+            />
+          </g>
+        </svg>
+      </Button>
+
       <Container className="text-center pb-2 text-body-secondary">
         Site Version: {appInfo.version}
       </Container>
